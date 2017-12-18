2f3fdda442d1998a8f442595abb59d8145809e1c
Conder <cs3544@columbia.edu>
2017-12-17 20:18:46 -0500

    scaffolding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routing } from './app.routing';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { DataSettingsComponent } from './data-settings/data-settings.component';
+import { QaComponent } from './qa/qa.component';
+import { PopupComponent } from './popup/popup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    DataSettingsComponent,
+    QaComponent,
+    PopupComponent
   ],
   imports: [
+    Routing,
     BrowserModule
   ],
   providers: [],
---
+++ src/app/app.routing.ts
@@ -0,0 +1,7 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+];
+
+export const Routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
